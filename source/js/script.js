b9d3e047812a0dd849ee4604945615abcb5b1ff0
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2020-12-23 17:10:42 +0300

    Стилизует подвал и добавляет кнопку вверх

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,18 @@
 'use strict';
 
 (() => {
-  return;
+  const MAX_OFFSET = 100;
+  const scrollButton = document.querySelector(`.scroll-button`);
+
+  scrollButton.addEventListener(`click`, () => {
+    window.scrollTo(0, 0);
+  });
+
+  window.addEventListener(`scroll`, () => {
+    if (window.pageYOffset < MAX_OFFSET) {
+      scrollButton.classList.add(`scroll-button--hidden`);
+    } else if (scrollButton.classList.contains(`scroll-button--hidden`)) {
+      scrollButton.classList.remove(`scroll-button--hidden`);
+    }
+  });
 })();
